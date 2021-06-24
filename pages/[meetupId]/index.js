7f6e6f46c9a44ec577f81a5b02e1023591a004af
haluskua <haluskua.one@gmail.com>
2021-06-25 01:04:02 +1200

    added detail.module.css file

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,17 @@
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
+function MeetupDetails() {
+  return (
+    <Fragment>
+      <MeetupDetail
+        image="https://cdn.pixabay.com/photo/2017/06/29/21/18/motorcycles-2456247_960_720.jpg"
+        title="First Meetup"
+        address="Some Street 3, Some City"
+        description="This is just the first meetup"
+      />
+    </Fragment>
+  );
+}
+
+export default MeetupDetails;
