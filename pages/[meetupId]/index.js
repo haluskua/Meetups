f6562c7972da4649442c5a0de044e3660ee411ad
haluskua <haluskua.one@gmail.com>
2021-06-25 12:54:27 +1200

    adde getStaticPaths()

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -14,4 +14,40 @@ function MeetupDetails() {
   );
 }
 
+//page is RE-GENERATE for all URL/ID during build up
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://cdn.pixabay.com/photo/2017/06/29/21/18/motorcycles-2456247_960_720.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
 export default MeetupDetails;
